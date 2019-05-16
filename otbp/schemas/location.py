b2b0a97240ce37e723adfc0eab5b686bbf7a7e4c
Tristan K <tristanmkernan@gmail.com>
2019-05-16 17:33:43 -0400

    feat: include geohash in location responses

--- otbp/schemas/location.py
+++ otbp/schemas/location.py
@@ -1,6 +1,7 @@
 import marshmallow
 
 from otbp.schemas import ma
+from otbp.utils.geohash import encode
 
 
 class LocationSchema(ma.Schema):
@@ -9,3 +10,12 @@ class LocationSchema(ma.Schema):
 
     lat = marshmallow.fields.Float(required=True)
     lng = marshmallow.fields.Float(required=True)
+    geohash = marshmallow.fields.Str(required=False)
+
+    @marshmallow.pre_dump
+    def pre_dump(self, loc):
+        return {
+            'lat': loc['lat'],
+            'lng': loc['lng'],
+            'geohash': encode(loc['lat'], loc['lng'])
+        }
